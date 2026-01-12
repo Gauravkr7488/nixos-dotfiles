1670a860a266627c7f1b7c4bd63661cdd897e0ac
Gaurav <gauravkr7488@gmail.com>
2026-01-12 11:52:06 +0530

    set default branch name to main

--- configs/git.nix
+++ configs/git.nix
@@ -4,5 +4,8 @@
                 enable = true;
                 userName = "gaurav";
                 userEmail = "gauravkr748800@gamil.com";
+                extraConfig = {
+                        init.defaultBranch = "main";
+                };
         };
 }
