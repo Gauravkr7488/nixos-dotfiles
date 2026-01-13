77f73b353babad69d30a535fe188bdfd2cf71e04
Gaurav <gauravkr7488@gmail.com>
2026-01-13 09:44:15 +0530

    changed theme and padding in alacritty

--- configs/alacritty.nix
+++ configs/alacritty.nix
@@ -1,12 +1,13 @@
 {
         programs.alacritty = {
                 enable = true;
-                theme = "gruvbox_dark";
+                theme = "miasma";
+                #theme = "inferno";
                 settings = {
                         window = {
                                 decorations = "None";
                                 opacity = 0.8;
-                                padding = { x = 4; y = 4; };
+                                padding = { x = 8; y = 8; };
                         };
                 };
         };
